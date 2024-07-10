5ed99cfd739c2549530bd29258fb990ec8ee16b2
Rupanshi Rohilla <114069039+rupanshirohilla1@users.noreply.github.com>
2024-07-10 16:46:00 +0530

    Update MDBFooter.jsx

--- src/components/core/HomePage/MDBFooter.jsx
+++ src/components/core/HomePage/MDBFooter.jsx
@@ -9,7 +9,7 @@ export default function MDBFooter() {
    <>
       <div className="w-[100%] flex flex-row text-md bg-gray-200 mt-16 text-center gap-4 items-center justify-center">
               <div className="text-center items-center">
-                Created By Vagish Maurya &copy; 2023
+                Created By Rupanshi Rohilla &copy; 2024
               </div>
               <div className="text-center items-center">
                 <HighLightText text={"EliteCoding.."}/>
